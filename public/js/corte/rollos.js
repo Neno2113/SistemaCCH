aa7c2ac38b0acb4774df9d4c54eb7eec19695c69
Anel Dominguez <anelgabriel002@gmail.com>
2021-10-26 16:49:37 -0400

    Error rollos

--- public/js/corte/rollos.js
+++ public/js/corte/rollos.js
@@ -224,7 +224,7 @@ $(document).ready(function() {
             responsive: true,
             ajax: "api/rollos",
             dom: 'Bfrtip',
-            iDisplayLength: 25,
+            iDisplayLength: 10,
             buttons: [
                 'pageLength',
                 'copyHtml5',
