7f03f80691972a911509b4e45f9daba5d5343a28
lccristobal <lccristbalbello@gmail.com>
2022-02-25 11:59:37 -0400

    Link to Dashboard en logo

--- resources/views/adminlte/menu.blade.php
+++ resources/views/adminlte/menu.blade.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="#" class="brand-link">
+    <a href="./home" class="brand-link">
         <img src="{{asset('/adminlte/img/AdminLTELogo.png')}}" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">SistemaCCH</span>
