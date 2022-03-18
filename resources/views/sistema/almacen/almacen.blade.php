7f43ae1cfa4edcea50706571daca4f898c6d5aa0
lccristobal <lccristbalbello@gmail.com>
2022-03-18 12:05:33 -0400

    faltaba cierre de select

--- resources/views/sistema/almacen/almacen.blade.php
+++ resources/views/sistema/almacen/almacen.blade.php
@@ -94,12 +94,13 @@
                             <button class="btn btn-dark btn-sm mb-2" id="btn-cat"  type="button"
                                 data-toggle="modal" data-target=".bd-ubicacion-modal-xl"><i class="fas fa-plus-square"></i></button>
                             </div>
-                            <!--
+                            
                             <div class="col-md-2 mb-2"> 
                                 <select name="ubicacion" id="ubicacion" class="form-control">
                                 <option value="" disabled>Ubicacion</option> 
+                                </select>
                             </div>
-        -->
+        
                         </div>
                         <div class="div-totales">
 
