4eadcee19e85224c72bf2430af20ba64687e5996
lccristobal <lccristobalbello@gmail.com>
2022-08-22 19:31:44 -0400

    ..

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -703,7 +703,7 @@ class EmpleadoController extends Controller
         $empleadoActual = Empleado::find($id)->get();
         $empleado_detalle  = EmpleadoDetalle::where('empleado_id', $id)->get();
 
-        $user_id = $empleado_detalle->('user_id');;
+        $user_id = $empleado_detalle->('user_id');
         $user = User::where('id', $user_id)->get();
 
     //    $user_id = $empleadoActual->user_id;
