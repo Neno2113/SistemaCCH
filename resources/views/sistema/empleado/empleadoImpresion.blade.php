1709bb620d8fca7a5a95bf16cc0efb38f41ef5b6
lccristobal <lccristobalbello@gmail.com>
2022-08-23 11:09:41 -0400

    ..

--- resources/views/sistema/empleado/empleadoImpresion.blade.php
+++ resources/views/sistema/empleado/empleadoImpresion.blade.php
@@ -4,7 +4,7 @@
 	<title>Hi</title>
 </head>
 <body>
-	<h1>Welcome to ItSolutionStuff.com - </h1>
+	<h1>Welcome to ItSolutionStuff.com - {{$empleadoActual->user->nombre}}</h1>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
