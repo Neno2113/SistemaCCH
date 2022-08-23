4e25777584bc376ef74a611554f7ce69359cb6d0
lccristobal <lccristobalbello@gmail.com>
2022-08-23 09:16:38 -0400

    ..

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -725,7 +725,7 @@ class EmpleadoController extends Controller
         )); */
 
         $data = ['title' => 'Welcome to ItSolutionStuff.com'];
-        $pdf = PDF::loadView('sistema.empleado.empleadoImpresion', $empleadoActual);
+        $pdf = PDF::loadView('sistema.empleado.empleadoImpresion', $empleado_detalle);
   
         return $pdf->download('prueba.pdf');
     //    return View('sistema.empleado.empleadoImpresion', $data);
