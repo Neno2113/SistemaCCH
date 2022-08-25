5b5e23fe91d693f35c62d048abc09f864de46567
lccristobal <lccristobalbello@gmail.com>
2022-08-25 12:49:00 -0400

    ..

--- resources/views/sistema/empleado/empleadoImpresion.blade.php
+++ resources/views/sistema/empleado/empleadoImpresion.blade.php
@@ -651,8 +651,8 @@
 	<main>
 		<div id="details" class="clearfix">
 			<div id="logo">
-			<!--	<img src="{{asset('adminlte/img/LOGO_CCH-01.jpg')}}"> -->
-				<img src="">
+				<img src="{{asset('adminlte/img/LOGO_CCH-01.jpg')}}"> 
+			<!--	<img src=""> -->
 			</div>
 			<table border="0" cellspacing="0" cellpadding="0" class="tabla-cliente">
 				<thead class="cod">
