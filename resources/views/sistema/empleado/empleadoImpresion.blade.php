6d52e0d4d6632d2b16e2e9bbbe079f6400fba873
lccristobal <lccristobalbello@gmail.com>
2022-08-25 13:20:49 -0400

    ..

--- resources/views/sistema/empleado/empleadoImpresion.blade.php
+++ resources/views/sistema/empleado/empleadoImpresion.blade.php
@@ -470,7 +470,7 @@
 			border-top: 1px solid black;
 			border-left: 1px solid black;
 			border-right: 1px solid black;
-			width: 40%; 
+			width: 60%; 
 			margin-right: 2%;
 			margin-left: 140px; 
 		}
