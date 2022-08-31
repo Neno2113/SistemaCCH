0eaadcc965b8d85eac0cdb47de96ea7990654ae8
lccristobal <lccristobalbello@gmail.com>
2022-08-31 09:17:26 -0400

    ..

--- resources/views/sistema/empleado/empleadoImpresion.blade.php
+++ resources/views/sistema/empleado/empleadoImpresion.blade.php
@@ -947,7 +947,7 @@
 			</table>
 		</div>
 		<div>
-			<h2 class="session">Formación Academica                    /                    Información Laboral</h2>
+			<h2 class="session">Formación Academica &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; / &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Información Laboral</h2>
 		</div>
 		<div id="details" class="clearfix">	
 			<table border="0" cellspacing="0" cellpadding="0" class="tabla-info" style="margin-right: 30px;">
