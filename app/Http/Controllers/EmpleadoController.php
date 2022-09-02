69898b34c9ffc219a9d1f2fc4cef05266ed35cd9
lccristobal <lccristobalbello@gmail.com>
2022-09-02 08:45:55 -0400

    ..

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -458,7 +458,7 @@ class EmpleadoController extends Controller
             
             if ($files = $request->input('avatar')) {
                 // Define upload path
-                    $destinationPath = public_path('/adminlte/img/'); // upload path
+                    $destinationPath = public_path('/adminlte/img/empleados/'); // upload path
                 // Upload Orginal Image           
                 //    $profileImage = date('YmdHis') . "." . $files->getClientOriginalExtension();
                     $imageName = time().'_'.$files;
