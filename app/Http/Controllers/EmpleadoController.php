7039dc2acbef7ce4c6f2d2f72cf064a5cc462972
lccristobal <lccristobalbello@gmail.com>
2022-09-08 08:50:09 -0400

    ..

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -895,12 +895,14 @@ class EmpleadoController extends Controller
         $no_evento_1 = "1";
         $evento_1 = $historico_laboral[0]->evento;
         $fecha_evento_1 = $historico_laboral[0]->fecha;
+        /*
         $no_evento_2 = "2";
         $evento_2 = $historico_laboral[1]->evento;
         $fecha_evento_2 = $historico_laboral[1]->fecha;
         $no_evento_3 = "3";
         $evento_3 = $historico_laboral[2]->evento;
         $fecha_evento_3 = $historico_laboral[2]->fecha;
+        */
 
         $nombre_esposa = $empleado_detalle->nombre_esposa;
         $telefono_esposa = $empleado_detalle->telefono_esposa;
