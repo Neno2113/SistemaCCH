26e5cf3d15fa76f201c716bd0472d243ba7e4569
lccristobal <lccristobalbello@gmail.com>
2022-09-09 08:26:15 -0400

    ..

--- resources/views/sistema/client/clients.blade.php
+++ resources/views/sistema/client/clients.blade.php
@@ -347,7 +347,7 @@
     <div class="col-10" id="registroFormBotones" style="margin-bottom: 10px;">
         <button class="btn btn-danger mt-2 float-right" id="btnCancelar"><i
         class="fas fa-arrow-alt-circle-left fa-lg"></i> Cancelar</button>
-        <button type="submit" id="btn-edit" class="btn btn-warning mt-2 float-right"><i
+        <button type="submit" id="btn-edit" class="btn btn-warning mt-2 float-right" style="margin-right:20px;"><i
                         class="far fa-edit fa-lg"></i> Editar</button>
         <button type="submit" id="btn-guardar" class="btn btn-primary mt-2 float-right" style="margin-right:20px;"><i
         class="far fa-save fa-lg"></i> Guardar</button>
