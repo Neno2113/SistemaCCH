4a4265b5a493dc0b678b406f365d3bee7434880b
lccristobal <lccristobalbello@gmail.com>
2022-09-12 12:47:21 -0400

    ..

--- app/Http/Controllers/SKUController.php
+++ app/Http/Controllers/SKUController.php
@@ -91,7 +91,7 @@ class SKUController extends Controller
             })
             ->editColumn('talla', function ($sku) {
                 $producto = $sku->producto_id;
-                $referencia = $sku->referencia_producto;
+            //    $referencia = $sku->referencia_producto;
             //    if($product = Product::where('id', $producto)->get()->first()) {
             //        $genero = $product->genero;   
                 //    $mujer_plus = Str::substr($referencia, 3, 4);
