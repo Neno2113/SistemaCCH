da311555b479c8a7c72868b5e9eddb8b8bfb7522
lccristobal <lccristobalbello@gmail.com>
2022-09-11 23:40:22 -0400

    ..

--- public/js/sku/sku.js
+++ public/js/sku/sku.js
@@ -107,7 +107,7 @@ $(document).ready(function() {
             ],
             order: [[3, 'desc']],
             rowGroup: {
-                dataSrc: 'referencia_producto'
+                dataSrc: ''
             }
         });
     }
