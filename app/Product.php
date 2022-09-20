f3bb3954d08ddd5a37633de648a8c717bb9cfa55
Cristobal <lccristobalbello@gmail.com>
2022-09-20 18:22:50 -0400

    ..

--- app/Product.php
+++ app/Product.php
@@ -11,7 +11,7 @@ class Product extends Model
     protected $fillable = [
         'id', 'id_user', 'referencia_producto', 'descripcion', 'ubicacion', 'imagen_frente', 'imagen_trasero',
         'imagen_perfil','imagen_bolsillo', 'tono', 'intensidad_proceso_seco', 'atributo_no_1', 'atributo_no_2',
-        'atributo_no_3', 'precio_lista', 'precio_venta_publico' 
+        'atributo_no_3', 'precio_lista', 'precio_venta_publico', 'entalle_bragueta', 'entalle_piernas'
     ];
 
     public function user()
