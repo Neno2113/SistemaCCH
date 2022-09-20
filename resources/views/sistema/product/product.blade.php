434bd1498818fefb2c1fd78f0589485c58ffbb86
Cristobal <lccristobalbello@gmail.com>
2022-09-20 17:53:07 -0400

    ..

--- resources/views/sistema/product/product.blade.php
+++ resources/views/sistema/product/product.blade.php
@@ -117,7 +117,7 @@
                         <div class="col-md-3 mt-4">
                             <label for="entalle_braqueta">Entalle de Bragueta</label>
                             <select name="entalle_braqueta" id="entalle_braqueta" class="form-control">
-                                <option value="" disabled>Entalle de Bragueta</option>
+                                <option value="" disabled selected>Entalle de Bragueta</option>
                                 <option value="20"><b>20</b></option>
                                 <option value="21"><b>21</b></option>
                                 <option value="22"><b>22</b></option>
@@ -135,7 +135,7 @@
                         <div class="col-md-3 mt-4">
                             <label for="entalle_piernas">Entalle de Entre Piernas</label>
                             <select name="entalle_piernas" id="entalle_piernas" class="form-control">
-                                <option value="" disabled>Entalle de Entre Piernas</option>
+                                <option value="" disabled selected>Entalle de Entre Piernas</option>
                                 <option value="20"><b>20</b></option>
                                 <option value="21"><b>21</b></option>
                                 <option value="22"><b>22</b></option>
