20c21e49583f9e32466c30a2dd65e6f0ae46e1ee
Cristobal <lccristobalbello@gmail.com>
2022-09-21 10:20:11 -0400

    ..

--- app/Http/Controllers/SKUController.php
+++ app/Http/Controllers/SKUController.php
@@ -47,7 +47,7 @@ class SKUController extends Controller
     public function skus()
     {
 
-        $skus = DB::table('sku')->join('corte', 'sku.producto_id', '=', 'corte..producto_id')
+        $skus = DB::table('sku')->join('corte', 'sku.producto_id', '=', 'corte.producto_id')
             ->select([
                 'sku.producto_id', 'corte.numero_corte', 'corte.fecha_corte', 'corte.no_marcada', 'sku.sku', 'sku.referencia_producto'
             ]);
