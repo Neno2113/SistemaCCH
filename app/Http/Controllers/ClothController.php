6fe4ce5aaaeed80dc7dfddb7b195f01453a8529f
Cristobal <lccristobalbello@gmail.com>
2022-09-28 11:35:24 -0400

    ..

--- app/Http/Controllers/ClothController.php
+++ app/Http/Controllers/ClothController.php
@@ -163,116 +163,11 @@ class ClothController extends Controller
 
     public function upload(Request $request)
     {
-        
-        $file = $request->file('rollo');
-        if ($file) {
-            $filename = $file->getClientOriginalName();
-            $extension = $file->getClientOriginalExtension(); //Get extension of uploaded file
-            $tempPath = $file->getRealPath();
-            $fileSize = $file->getSize(); //Get size of uploaded file in bytes
-
- 
-            //Check for file extension and size
-         //   $this->checkUploadedFileProperties($extension, $fileSize);
-            //Where uploaded file will be stored on the server 
-            $location = public_path().'/uploads'; //Created an "uploads" folder for that
-            // Upload file
-            $file->move($location, $filename);
-
-            // In case the uploaded file path is to be stored in the database 
-        //    $filepath = public_path($location . "/" . $filename);
-            $filepath = $location."/".$filename;
-            // Reading file
-            $file = fopen($filepath, "r");
-            $importData_arr = array(); // Read through the file and store the contents as an array
-            $i = 0;
-
-            //Read the contents of the uploaded file 
-            while (($filedata = fgetcsv($file, 1000, ";")) !== FALSE) {
-                $num = count($filedata);
-                // Skip first row (Remove below comment if you want to skip the first row)
-                    if ($i == 0) {
-                    $i++;
-                    continue;
-                    }
-                for ($c = 0; $c < $num; $c++) {
-                    $importData_arr[$i][] = $filedata[$c];
-                }
-                    $i++;
-            }
-            fclose($file); //Close after reading
-
-            $j = 0;
-            foreach ($importData_arr as $importData) {
-                $numero = $importData[0]; //Get user names
-                $tono = $importData[1]; //Get user names
-                $longitud = $importData[2]; //Get the user emails
-                $j++;
-
-                try {
-                    
-                    $rollos_detail = new RollosDetail();
-
-                    $rollos_detail->id_rollo = '61';
-                    $rollos_detail->id_tela = '24';
-                    $rollos_detail->numero = $numero;
-                    $rollos_detail->tono = $tono;
-                    $rollos_detail->longitud = $longitud;
-
-                    $rollos_detail->save();
-
-
-                    ////////////////////////////////////
-                    /*
-                    DB::beginTransaction();
-                    Player::create([
-                    'name' => $importData[1],
-                    'club' => $importData[2],
-                    'email' => $importData[3],
-                    'position' => $importData[4],
-                    'age' => $importData[5],
-                    'salary' => $importData[6]
-                    ]);
-                    //Send Email
-                //    $this->sendEmail($email, $name);
-                    DB::commit();
-                    */
-                
-                } catch (\Exception $e) {
-                    //throw $th;
-                //    DB::rollBack();
-                }
-            }
-
-                $data = [
-                    'code' => 200,
-                    'status' => 'success',
-                    'location' => $location,
-                    'filepath' => $importData_arr[1][0]
-                ];
-
-            /*
-            return response()->json([
-                'message' => "$j records successfully uploaded"
-                ]); */
-                
-        } else {
-        //no file was uploaded
-       // throw new \Exception('No file was uploaded', Response::HTTP_BAD_REQUEST);
-        }
-          
-        
-
-    //    Excel::import(new ImportRollos, request()->file('rollo'));
-    //    Excel::import(new ImportRollos, $request->file('rollo')); 
-    //    return back();
-        /*
-        //validar el archivo
-        $validate = \Validator::make($request->all(), [
-            'rollo' => 'required|in:xlsx,xls',
+         //validar el archivo
+         $validate = \Validator::make($request->all(), [
+            'rollo' => 'required|in:csv'
         ]);
 
-        // Guardar el archivo
         if ($validate->fails()) {
             $data = [
                 'code' => 404,
@@ -281,37 +176,82 @@ class ClothController extends Controller
             ];
         } else {
 
-           
-         
+            $file = $request->file('rollo');
+            if ($file) {
+                $filename = $file->getClientOriginalName();
+                $extension = $file->getClientOriginalExtension(); //Get extension of uploaded file
+                $tempPath = $file->getRealPath();
+                $fileSize = $file->getSize(); //Get size of uploaded file in bytes
 
-        //    $avatar = $request->file('avatar');
-        //    $image_name_1 = time() . $avatar->getClientOriginalName();
-            // echo $id;
-            // die();
-        //    if (!empty($avatar)) {
-        //    \Storage::disk('avatar')->put($image_name_1, \File::get($avatar));
-        //    $destinationPath = public_path().'/adminlte/img';
-        //    $avatar->move($destinationPath,$image_name_1);
-            
-           } else {
-                $data = [
-                    'code' => 400,
-                    'status' => 'error',
-                    'message' => 'WTF'
-                ];
+    
+                //Check for file extension and size
+            //   $this->checkUploadedFileProperties($extension, $fileSize);
+                //Where uploaded file will be stored on the server 
+                $location = public_path().'/uploads'; //Created an "uploads" folder for that
+                // Upload file
+                $file->move($location, $filename);
+
+                // In case the uploaded file path is to be stored in the database 
+            //    $filepath = public_path($location . "/" . $filename);
+                $filepath = $location."/".$filename;
+                // Reading file
+                $file = fopen($filepath, "r");
+                $importData_arr = array(); // Read through the file and store the contents as an array
+                $i = 0;
+
+                //Read the contents of the uploaded file 
+                while (($filedata = fgetcsv($file, 1000, ";")) !== FALSE) {
+                    $num = count($filedata);
+                    // Skip first row (Remove below comment if you want to skip the first row)
+                        if ($i == 0) {
+                        $i++;
+                        continue;
+                        }
+                    for ($c = 0; $c < $num; $c++) {
+                        $importData_arr[$i][] = $filedata[$c];
+                    }
+                        $i++;
+                }
+                fclose($file); //Close after reading
+
+                $j = 0;
+                foreach ($importData_arr as $importData) {
+                    $numero = $importData[0]; //Get user names
+                    $tono = $importData[1]; //Get user names
+                    $longitud = $importData[2]; //Get the user emails
+                    $j++;
+
+                    try {
+                        
+                        $rollos_detail = new RollosDetail();
+
+                        $rollos_detail->id_rollo = '61';
+                        $rollos_detail->id_tela = '24';
+                        $rollos_detail->numero = $numero;
+                        $rollos_detail->tono = $tono;
+                        $rollos_detail->longitud = $longitud;
+
+                        $rollos_detail->save();
+                    
+                    } catch (\Exception $e) {
+                        //throw $th;
+                    //    DB::rollBack();
+                    }
+                }
+
+                    $data = [
+                        'code' => 200,
+                        'status' => 'success',
+                        'location' => $location,
+                        'filepath' => $j
+                    ];
+
+                    
+            } else {
+            //no file was uploaded
+            throw new \Exception('No file was uploaded', Response::HTTP_BAD_REQUEST);
             }
-        
-
-            $data = [
-                'code' => 200,
-                'status' => 'success',
-                'probar' => 'funcioando'
-            ];
-            
-            
         }
-        
-        */
         return response()->json($data, $data['code']);
         
     }
