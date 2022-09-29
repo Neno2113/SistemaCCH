29c80a21b5497f23d9f323a7494992af49b3b2a4
Cristobal <lccristobalbello@gmail.com>
2022-09-29 09:27:38 -0400

    ..

--- app/Http/Controllers/ClothController.php
+++ app/Http/Controllers/ClothController.php
@@ -164,7 +164,8 @@ class ClothController extends Controller
 
     public function upload(Request $request)
     {
-        $file = $request->file('rollo');
+        $status = "success";
+        $file = $request->file('rollo');  
         if ($file) {
             $filename = $file->getClientOriginalName();
             $extension = $file->getClientOriginalExtension(); //Get extension of uploaded file
@@ -174,70 +175,83 @@ class ClothController extends Controller
 
             //Check for file extension and size
             $this->checkUploadedFileProperties($extension, $fileSize);
-            //Where uploaded file will be stored on the server 
-            $location = public_path().'/uploads'; //Created an "uploads" folder for that
- /*           // Upload file
-            $file->move($location, $filename);
+            if ($status == "success"){
 
-            // In case the uploaded file path is to be stored in the database 
-        //    $filepath = public_path($location . "/" . $filename);
-            $filepath = $location."/".$filename;
-            // Reading file
-            $file = fopen($filepath, "r");
-            $importData_arr = array(); // Read through the file and store the contents as an array
-            $i = 0;
+                //Where uploaded file will be stored on the server 
+                $location = public_path().'/uploads'; //Created an "uploads" folder for that
+                // Upload file
+                $file->move($location, $filename);
 
-            //Read the contents of the uploaded file 
-            while (($filedata = fgetcsv($file, 1000, ";")) !== FALSE) {
-                $num = count($filedata);
-                // Skip first row (Remove below comment if you want to skip the first row)
-                    if ($i == 0) {
-                    $i++;
-                    continue;
+                // In case the uploaded file path is to be stored in the database 
+ /               //    $filepath = public_path($location . "/" . $filename);
+                $filepath = $location."/".$filename;
+                // Reading file
+                $file = fopen($filepath, "r");
+                $importData_arr = array(); // Read through the file and store the contents as an array
+                $i = 0;
+
+                //Read the contents of the uploaded file 
+                while (($filedata = fgetcsv($file, 1000, ";")) !== FALSE) {
+                    $num = count($filedata);
+                    // Skip first row (Remove below comment if you want to skip the first row)
+                        if ($i == 0) {
+                        $i++;
+                        continue;
+                        }
+                    for ($c = 0; $c < $num; $c++) {
+                        $importData_arr[$i][] = $filedata[$c];
                     }
-                for ($c = 0; $c < $num; $c++) {
-                    $importData_arr[$i][] = $filedata[$c];
+                        $i++;
                 }
-                    $i++;
-            }
-            fclose($file); //Close after reading
+                fclose($file); //Close after reading
 
-            $j = 0;
-            foreach ($importData_arr as $importData) {
-                $numero = $importData[0]; //Get user names
-                $tono = $importData[1]; //Get user names
-                $longitud = $importData[2]; //Get the user emails
-                $j++;
+                $j = 0;
+                foreach ($importData_arr as $importData) {
+                    $numero = $importData[0]; //Get user names
+                    $tono = $importData[1]; //Get user names
+                    $longitud = $importData[2]; //Get the user emails
+                    $j++;
 
-                try {
+                    try {
+                        
+                        $rollos_detail = new RollosDetail();
+
+                        $rollos_detail->id_rollo = '61';
+                        $rollos_detail->id_tela = '24';
+                        $rollos_detail->numero = $numero;
+                        $rollos_detail->tono = $tono;
+                        $rollos_detail->longitud = $longitud;
+
+                        $rollos_detail->save();
                     
-                    $rollos_detail = new RollosDetail();
-
-                    $rollos_detail->id_rollo = '61';
-                    $rollos_detail->id_tela = '24';
-                    $rollos_detail->numero = $numero;
-                    $rollos_detail->tono = $tono;
-                    $rollos_detail->longitud = $longitud;
-
-                    $rollos_detail->save();
-                
-                } catch (\Exception $e) {
-                    //throw $th;
-                //    DB::rollBack();
+                    } catch (\Exception $e) {
+                        //throw $th;
+                    //    DB::rollBack();
+                    }
                 }
-            }
-*/
                 $data = [
                     'code' => 200,
                     'status' => 'success',
                     'location' => $location,
                     'filepath' => $fileSize
                 ];
+            } elseif ($status == "file-very-large" ){
+                $data = [
+                    'code' => 200,
+                    'status' => 'file-very-large'
+                ];
 
+            } else {
+                $data = [
+                    'code' => 200,
+                    'status' => 'file-no-valid'
+                ];
                 
+            }
+      
         } else {
         //no file was uploaded
-        throw new \Exception('No file was uploaded', Response::HTTP_BAD_REQUEST);
+    //    throw new \Exception('No file was uploaded', Response::HTTP_BAD_REQUEST);
         }
 
         return response()->json($data, $data['code']); 
@@ -251,10 +265,12 @@ class ClothController extends Controller
        if (in_array(strtolower($extension), $valid_extension)) {
             if ($fileSize <= $maxFileSize) {
             } else {
-                throw new \Exception('No file was uploaded', Response::HTTP_REQUEST_ENTITY_TOO_LARGE); //413 error
+                $status = "file-very-large";
+            //    throw new \Exception('No file was uploaded', Response::HTTP_REQUEST_ENTITY_TOO_LARGE); //413 error
             }
         } else {
-            throw new \Exception('Invalid file extension', Response::HTTP_UNSUPPORTED_MEDIA_TYPE); //415 error
+            $status = "file-no-valid";
+         //   throw new \Exception('Invalid file extension', Response::HTTP_UNSUPPORTED_MEDIA_TYPE); //415 error
         }
     }
 
