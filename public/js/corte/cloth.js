50dcf22e799ba4ae71983e74434f7675b754c116
Cristobal <lccristobalbello@gmail.com>
2022-10-03 09:11:43 -0400

    ..

--- public/js/corte/cloth.js
+++ public/js/corte/cloth.js
@@ -288,8 +288,10 @@ $(document).ready(function() {
     $("#btn-terminar").click(function(e){
         e.preventDefault();
 
-    //    limpiar();
-    //    tabla.ajax.reload();
+        limpiar();
+        tabla.ajax.reload();
+        listar();
+        tipoTelas();
         mostrarForm(false);
         $("#btn-composicion").removeClass("btn-success").addClass("btn-orange");
         $("#btn-upload").attr("disabled", true);
