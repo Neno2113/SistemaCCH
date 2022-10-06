a4adb359cd91326907923b116dded5f67b3587f0
Cristobal <lccristobalbello@gmail.com>
2022-10-06 11:35:50 -0400

    ..

--- app/Http/Controllers/SKUController.php
+++ app/Http/Controllers/SKUController.php
@@ -342,7 +342,7 @@ class SKUController extends Controller
         $data = ['nombre' => 'Etiqueta', 'apellido' => 'Lavish'];
         $pdf = PDF::loadView('sistema.sku.skuImpresion', $data);
   
-        return $pdf->stream('Etiquetas-123.pdf');
+        return $pdf->print('Etiquetas-123.pdf');
     //    return view('sistema.sku.skuImpresion');
 
 
