cd7adb24d92212c9f9c3ab873f10ad606bf15db9
Cristobal <lccristobalbello@gmail.com>
2022-10-07 12:47:19 -0400

    ..

--- app/Http/Controllers/SKUController.php
+++ app/Http/Controllers/SKUController.php
@@ -342,8 +342,190 @@ class SKUController extends Controller
         $skus = SKU::find($id);
         $producto = Product::where('id', $skus->producto_id)->get()->first();
         $corte = Corte::where('producto_id', $skus->producto_id)->get()->first();
+        $bragueta = CategoriaProducto::where('tipo', 'entalle_bragueta')->where('indice', $producto->entalle_bragueta)->get()->first();
+            $entalle_bragueta = $bragueta->nombre;
+        $pierna = CategoriaProducto::where('tipo', 'entalle_piernas')->where('indice', $producto->entalle_piernas)->get()->first();
+            $entalle_piernas = $bragueta->nombre;
 
-        $data = ['referencia' => $skus->referencia_producto, 'sku' => $skus->sku, 'talla' => $skus->talla, 'entalle_bragueta' => $producto->entalle_bragueta, 'entalle_piernas' => $producto->entalle_piernas, 'fecha_corte' => $corte->fecha_corte];
+    
+            $referencia2 = $producto->referencia_producto_2;
+            $referencia1 = $producto->referencia_producto;
+            $genero = $producto->genero;   
+            $mujer_plus = substr($referencia, 3, 1);
+            $min_talla = $producto->min;   
+            $max_talla = $producto->max;   
+            
+            if ($genero == "2") {
+                if ($mujer_plus == "7") {
+                    $a = '12W';
+                    $b = '14W';
+                    $c = '16W';
+                    $d = '18W';
+                    $e = '20W';
+                    $f = '22W';
+                    $g = '24W';
+                    $h = '26W';
+                } else {
+                    $a = '0/0';
+                    $b = '1/2';
+                    $c = '3/4';
+                    $d = '5/6';
+                    $e = '7/8';
+                    $f = '9/10';
+                    $g = '11/12';
+                    $h = '13/14';
+                    $i = '15/16';
+                    $j = '17/18';
+                    $k = '19/20';
+                    $l = '21/22';
+                }
+            } else if ($genero == "3" || $genero == "4") {
+                    $a = '2';
+                    $b = '4';
+                    $c = '6';
+                    $d = '8';
+                    $e = '10';
+                    $f = '12';
+                    $g = '14';
+                    $h = '16';
+                if ($referencia == $referencia1) {
+                    switch ($producto->min) {
+                        case "a":
+                            $min_talla = "";
+                            $max_talla = "";
+                            break;
+                        case "b":
+                            $min_talla = "a";
+                            $max_talla = "a";
+                            break;
+                        case "c":
+                            $min_talla = "a";
+                            $max_talla = "b";
+                            break;
+                        case "d":
+                            $min_talla = "a";
+                            $max_talla = "c";
+                            break;
+                        case "e":
+                            $min_talla = "a";
+                            $max_talla = "d";
+                            break;
+                        case "f":
+                            $min_talla = "a";
+                            $max_talla = "e";
+                            break;
+                        case "g":
+                            $min_talla = "a";
+                            $max_talla = "f";
+                            break;
+                        case "h":
+                            $min_talla = "a";
+                            $max_talla = "g";
+                            break;
+                    }
+
+                }
+            } else if ($genero == "1") {
+                    $a = '28';
+                    $b = '29';
+                    $c = '30';
+                    $d = '32';
+                    $e = '34';
+                    $f = '36';
+                    $g = '38';
+                    $h = '40';
+                    $i = '42';
+                    $j = '44';
+                    $k = '46';
+            }
+
+            if ($talla == "General") {
+                if ($min_talla == 'a') {
+                    $min_talla = $a;
+                } else if ($min_talla == 'b') {
+                    $min_talla = $b;
+                } else if ($min_talla == 'c') {
+                    $min_talla = $c;
+                } else if ($min_talla == 'd') {
+                    $min_talla = $d;
+                } else if ($min_talla == 'e') {
+                    $min_talla = $e;
+                } else if ($min_talla == 'f') {
+                    $min_talla = $f;
+                } else if ($min_talla == 'g') {
+                    $min_talla = $g;
+                } else if ($min_talla == 'h') {
+                    $min_talla = $h;
+                } else if ($min_talla == 'i') {
+                    $min_talla = $i;
+                } else if ($min_talla == 'j') {
+                    $min_talla = $j;
+                } else if ($min_talla == 'k') {
+                    $min_talla = $k;
+                } else if ($min_talla == 'l') {
+                    $min_talla = $l;
+                } 
+
+                if ($max_talla == 'a') {
+                    $max_talla = $a;
+                } else if ($max_talla == 'b') {
+                    $max_talla = $b;
+                } else if ($max_talla == 'c') {
+                    $max_talla = $c;
+                } else if ($max_talla == 'd') {
+                    $max_talla = $d;
+                } else if ($max_talla == 'e') {
+                    $max_talla = $e;
+                } else if ($max_talla == 'f') {
+                    $max_talla = $f;
+                } else if ($max_talla == 'g') {
+                    $max_talla = $g;
+                } else if ($max_talla == 'h') {
+                    $max_talla = $h;
+                } else if ($max_talla == 'i') {
+                    $max_talla = $i;
+                } else if ($max_talla == 'j') {
+                    $max_talla = $j;
+                } else if ($max_talla == 'k') {
+                    $max_talla = $k;
+                } else if ($max_talla == 'l') {
+                    $max_talla = $l;
+                } 
+
+            //    return $min_talla. ' - '.$max_talla;
+                $talla = $min_talla. ' - '.$max_talla;
+            } else {
+                if ($talla == 'A') {
+                    $talla = $a;
+                } else if ($talla == 'B') {
+                    $talla = $b;
+                } else if ($talla == 'C') {
+                    $talla = $c;
+                } else if ($talla == 'D') {
+                    $talla = $d;
+                } else if ($talla == 'E') {
+                    $talla = $e;
+                } else if ($talla == 'F') {
+                    $talla = $f;
+                } else if ($talla == 'G') {
+                    $talla = $g;
+                } else if ($talla == 'H') {
+                    $talla = $h;
+                } else if ($talla == 'I') {
+                    $talla = $i;
+                } else if ($talla == 'J') {
+                    $talla = $j;
+                } else if ($talla == 'K') {
+                    $talla = $k;
+                } else if ($talla == 'L') {
+                    $talla = $l;
+                } 
+
+            //    return $talla;
+
+            }  
+
+        $data = ['referencia' => $skus->referencia_producto, 'sku' => $skus->sku, 'talla' => $talla, 'entalle_bragueta' => $entalle_bragueta, 'entalle_piernas' => $entalle_piernas, 'fecha_corte' => $corte->fecha_corte];
     //    $pdf = PDF::loadView('sistema.sku.skuImpresion', $data);
   
     //    return $pdf->stream('Etiquetas-123.pdf');
