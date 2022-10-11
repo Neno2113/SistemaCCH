3afa2002d29454aa941c744a37175be74a11060b
Cristobal <lccristobalbello@gmail.com>
2022-10-11 09:53:33 -0400

    ..

--- resources/views/sistema/sku/skuImpresion.blade.php
+++ resources/views/sistema/sku/skuImpresion.blade.php
@@ -385,7 +385,7 @@
 	<table border="0" cellpadding="0" cellspacing="0" class="image_block block-1" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
 	<tr>
 	<td class="pad" style="width:100%;padding-right:0px;padding-left:0px;">
-	<div align="center" class="alignment" style="line-height:10px"><img src="{{ $sku }}" style="display: block; height: auto; border: 0; max-width: 100%;"/></div>
+	<div align="center" class="alignment" style="line-height:10px"><img src="" style="display: block; height: auto; border: 0; max-width: 100%;"/></div>
 	</td>
 	</tr>
 	</table>
@@ -408,7 +408,7 @@
 	<table border="0" cellpadding="0" cellspacing="0" class="html_block block-1" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
 	<tr>
 	<td class="pad">
-	<div align="center" style="font-family:Cabin, Arial, Helvetica Neue, Helvetica, sans-serif;text-align:center;"><div class="our-class"> SUGGESTED RETAIL PRICE </div></div>
+	<div align="center" style="font-family:Cabin, Arial, Helvetica Neue, Helvetica, sans-serif;text-align:center;"><div class="our-class"> {{ $sku }} SUGGESTED RETAIL PRICE </div></div>
 	</td>
 	</tr>
 	</table>
