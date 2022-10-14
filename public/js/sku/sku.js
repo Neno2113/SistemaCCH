73dda7ce5b7bad5125c7bd73cd8e0756d87c8196
Cristobal <lccristobalbello@gmail.com>
2022-10-14 09:06:52 -0400

    ..

--- public/js/sku/sku.js
+++ public/js/sku/sku.js
@@ -220,7 +220,7 @@ function mostrar(id_sku) {
             "<td class='font-weight-bold'>"+data.sku.referencia_producto+"</td>"+
             "<td class='font-weight-bold'>"+data.sku.talla+"</td>"+
             "<td class='font-weight-bold'><input type='number' placeholder='Cantidad' name='cantidad' id='cantidad' value='10'></td>"+
-            "<td><button type='button' id='btn-print' class='btn btn-danger'><i class='far fa-print'></i></button></td>"+
+            "<td><button type='button' id='btn-print' class='btn btn-danger'><i class='fas fa-print'></i></button></td>"+
             "</tr>";
             $("#permisos-agregados").append(fila);
         //    $("#id").val(data.tela.id);
@@ -233,7 +233,7 @@ function mostrar(id_sku) {
                 "<td class=''>"+data.skus[i].referencia_producto+"</td>"+
                 "<td class=''>"+data.skus[i].talla+"</td>"+
                 "<td class=''><input type='number' placeholder='Cantidad' name='cantidad' id='cantidad' value='10'></td>"+
-                "<td><button type='button' id='btn-print' class='btn btn-danger'><i class='far fa-print'></i></button></td>"+
+                "<td><button type='button' id='btn-print' class='btn btn-danger'><i class='fas fa-print'></i></button></td>"+
                 "</tr>";
                 $("#permisos-agregados").append(fila);
             }
