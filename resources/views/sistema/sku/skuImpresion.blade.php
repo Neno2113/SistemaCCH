419006e9ae611acf1ae592148b4c133988bfa557
Cristobal <lccristobalbello@gmail.com>
2022-10-21 09:28:32 -0400

    ..

--- resources/views/sistema/sku/skuImpresion.blade.php
+++ resources/views/sistema/sku/skuImpresion.blade.php
@@ -129,10 +129,10 @@
 	<table align="center" border="0" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 250px;" width="2500">
 	<tbody>
 	<tr>
-	<td  style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="100%">
+	<td  style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 0px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="100%">
 	<table border="0" cellpadding="5" cellspacing="0" class="image_block block-1" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
 	<tr>
-	<td class="pad">
+	<td class="pad" style="padding-top: 2px; padding-bottom: 2px;">
 	<div align="center" class="alignment" style="line-height:10px"><img alt="Logo" src="{{ '../avatar/1665404934lavish_logo.jpg' }}" style="display: block; height: auto; border: 0; width: 80px; max-width: 100%;" title="Logo" width="140"/></div>
 	</td>
 	</tr>
@@ -202,7 +202,7 @@
 	<td class="column column-2" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="50%">
 	<table border="0" cellpadding="0" cellspacing="0" class="paragraph_block block-2" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
 	<tr>
-	<td class="pad" style="padding-top:5px;">
+	<td class="pad" style="/* padding-top:5px; */">
 	<div style="color:#000000;font-size:8px;font-family:Cabin, Arial, Helvetica Neue, Helvetica, sans-serif;font-weight:400;line-height:120%;text-align:center;direction:ltr;letter-spacing:0px;mso-line-height-alt:16.8px;">
 	<p style="margin: 0;">{{ $entalle_bragueta }}</p>
 	</div>
@@ -211,7 +211,7 @@
 	</table>
 	<table border="0" cellpadding="0" cellspacing="0" class="paragraph_block block-3" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
 	<tr>
-	<td class="pad" style="padding-bottom:5px;">
+	<td class="pad" style="/* padding-bottom:5px; */">
 	<div style="color:#000000;font-size:8px;font-family:Cabin, Arial, Helvetica Neue, Helvetica, sans-serif;font-weight:400;line-height:120%;text-align:center;direction:ltr;letter-spacing:0px;mso-line-height-alt:16.8px;">
 	<p style="margin: 0;">{{ $entalle_piernas }}</p>
 	</div>
@@ -400,7 +400,7 @@
 	<table align="center" border="0" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #ffffff; color: #000000; border-radius: 0; width: 110px;" width="110">
 	<tbody>
 	<tr>
-	<td  style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="100%">
+	<td  style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 0px; padding-bottom: 0px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="100%">
 	<table border="0" cellpadding="0" cellspacing="0" class="html_block block-1" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
 	<tr>
 	<td class="pad">
