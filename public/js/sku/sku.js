320914118b81a332474bfcd7173c007759554ae4
Cristobal <lccristobalbello@gmail.com>
2022-10-26 10:24:17 -0400

    ..

--- public/js/sku/sku.js
+++ public/js/sku/sku.js
@@ -91,8 +91,8 @@ $(document).ready(function() {
                         "<td class=''>"+data.skus[i].sku_especial+"</td>"+
                         "<td class='' id='referencia_product'>"+data.skus[i].referencia_producto+"</td>"+
                         "<td class=''>"+data.skus[i].talla+"</td>"+
-                        "<td class=''><input type='number' class='text-center' placeholder='Cantidad' name='cantidad' id='cantidad"+i+"' value='"+data.skus[i].cantidad+"'></td>"+
-                        "<td><a href='print_label/"+data.skus[i].id+"/"+data.skus[i].cantidad+"/especial' target='_blank' id='enlaceprint"+i+"' onclick='redirigir("+i+","+data.skus[i].id+","+2+");'class='btn btn-primary ml-1'> <i class='fas fa-print'></i></a></td>"+
+                        "<td class=''><input type='number' class='text-center' placeholder='Cantidad' name='cantidad' id='cantidade"+i+"' value='"+data.skus[i].cantidad+"'></td>"+
+                        "<td><a href='print_label/"+data.skus[i].id+"/"+data.skus[i].cantidad+"/especial' target='_blank' id='enlaceprinte"+i+"' onclick='redirigir(e"+i+","+data.skus[i].id+","+2+");'class='btn btn-primary ml-1'> <i class='fas fa-print'></i></a></td>"+
                         "</tr>";
                         $("#permisos-agregados").append(fila);
                     }
@@ -148,8 +148,8 @@ $(document).ready(function() {
                         "<td class=''>"+datos.skus_esp[i].sku_especial+"</td>"+
                         "<td class=''>"+datos.skus_esp[i].referencia_producto+"</td>"+
                         "<td class=''>"+datos.skus_esp[i].talla+"</td>"+
-                        "<td class=''><input type='number' class='text-center' placeholder='Cantidad' name='cantidad' id='cantidad0' value='"+datos.skus_esp[i].cantidad+"'></td>"+
-                        "<td><a href='print_label/"+datos.skus_esp[i].id+"/"+datos.skus_esp[i].cantidad+"/especial' target='_blank' id='enlaceprint"+i+"' onclick='redirigir("+i+","+datos.skus_esp[i].id+","+2+");' class='btn btn-primary ml-1'> <i class='fas fa-print'></i></a></td>"+
+                        "<td class=''><input type='number' class='text-center' placeholder='Cantidad' name='cantidad' id='cantidade"+i+"' value='"+datos.skus_esp[i].cantidad+"'></td>"+
+                        "<td><a href='print_label/"+datos.skus_esp[i].id+"/"+datos.skus_esp[i].cantidad+"/especial' target='_blank' id='enlaceprinte"+i+"' onclick='redirigir(e"+i+","+datos.skus_esp[i].id+","+2+");' class='btn btn-primary ml-1'> <i class='fas fa-print'></i></a></td>"+
                         "</tr>";
                         $("#permisos-agregados").append(fila);
                     }
@@ -413,7 +413,7 @@ function mostrar(id_sku) {
                 "<td class='font-weight-bold' id='referencia_product'><input type='hidden' name='prod_id' id='prod_id' value='"+data.sku.producto_id+"'>"+data.sku.referencia_producto+"</td>"+
                 "<td class='font-weight-bold'>"+data.sku.talla+"</td>"+
                 "<td class='font-weight-bold'><input type='number' class='text-center' placeholder='Cantidad' name='cantidad' id='cantidad0' value='"+total+"'></td>"+
-                "<td><a href='print_label/"+data.sku.id+"/"+total+"/generall' target='_blank' id='enlaceprint0' onclick='redirigir(0,"+data.sku.id+",1);' class='btn btn-primary ml-1'> <i class='fas fa-print'></i></a></td>"+
+                "<td><a href='print_label/"+data.sku.id+"/"+total+"/general' target='_blank' id='enlaceprint0' onclick='redirigir(0,"+data.sku.id+",1);' class='btn btn-primary ml-1'> <i class='fas fa-print'></i></a></td>"+
                 "</tr>";
                 $("#permisos-agregados").append(fila);
                 
