dde2ec49729fa50011dab330763e805c4e83e3df
Cristobal <lccristobalbello@gmail.com>
2022-10-28 11:35:16 -0400

    ..

--- resources/views/sistema/sku/skuImpresion.blade.php
+++ resources/views/sistema/sku/skuImpresion.blade.php
@@ -157,7 +157,7 @@
 		<td  style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="50%">
 		<table border="0" cellpadding="0" cellspacing="0" class="paragraph_block block-2" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
 		<tr>
-		<td class="pad" style="/* padding-top:5px; */padding-bottom:2px;">
+		<td class="pad" style="padding-left:5px; padding-bottom:2px;">
 		<div style="color:#000000;font-size:9px;font-family:Arial, Helvetica Neue, Helvetica, sans-serif;font-weight:400;line-height:120%;text-align:left;direction:ltr;letter-spacing:0px;mso-line-height-alt:16.8px;">
 		<p style="margin: 0;">REFERENCE:</p>
 		</div>
@@ -193,7 +193,7 @@
 		<td  style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="50%">
 		<table border="0" cellpadding="0" cellspacing="0" class="paragraph_block block-2" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
 		<tr>
-		<td class="pad" style="/* padding-top:5px; */padding-bottom:2px;">
+		<td class="pad" style="padding-left:5px; padding-bottom:2px;">
 		<div style="color:#000000;font-size:9px;font-family:Arial, Helvetica Neue, Helvetica, sans-serif;font-weight:400;line-height:120%;text-align:left;direction:ltr;letter-spacing:0px;mso-line-height-alt:16.8px;">
 		<p style="margin: 0;">FIT:</p>
 		</div>
@@ -238,7 +238,7 @@
 		<td  style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="50%">
 		<table border="0" cellpadding="0" cellspacing="0" class="paragraph_block block-2" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
 		<tr>
-		<td class="pad" style="/* padding-top:5px; */padding-bottom:2px;">
+		<td class="pad" style="padding-left:5px; padding-bottom:2px;">
 		<div style="color:#000000;font-size:9px;font-family:Arial, Helvetica Neue, Helvetica, sans-serif;font-weight:400;line-height:120%;text-align:left;direction:ltr;letter-spacing:0px;mso-line-height-alt:16.8px;">
 		<p style="margin: 0;">SEASON:</p>
 		</div>
@@ -274,7 +274,7 @@
 		<td  style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="50%">
 		<table border="0" cellpadding="0" cellspacing="0" class="paragraph_block block-2" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
 		<tr>
-		<td class="pad" style="/* padding-top:5px; */padding-bottom:2px;">
+		<td class="pad" style="padding-left:5px; padding-bottom:2px;">
 		<div style="color:#000000;font-size:9px;font-family:Arial, Helvetica Neue, Helvetica, sans-serif;font-weight:400;line-height:120%;text-align:left;direction:ltr;letter-spacing:0px;mso-line-height-alt:16.8px;">
 		<p style="margin: 0;">FABRIC NO:</p>
 		</div>
@@ -310,7 +310,7 @@
 		<td  style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="50%">
 		<table border="0" cellpadding="0" cellspacing="0" class="paragraph_block block-2" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
 		<tr>
-		<td class="pad" style="/* padding-top:5px; */padding-bottom:2px;">
+		<td class="pad" style="padding-left:5px; padding-bottom:2px;">
 		<div style="color:#000000;font-size:9px;font-family:Arial, Helvetica Neue, Helvetica, sans-serif;font-weight:400;line-height:120%;text-align:left;direction:ltr;letter-spacing:0px;mso-line-height-alt:16.8px;">
 		<p style="margin: 0;">WASH NO:</p>
 		</div>
@@ -346,7 +346,7 @@
 		<td  style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="50%">
 		<table border="0" cellpadding="0" cellspacing="0" class="paragraph_block block-2" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
 		<tr>
-		<td class="pad" style="/* padding-top:5px; */padding-bottom:2px;">
+		<td class="pad" style="padding-left:5px; padding-bottom:2px;">
 		<div style="color:#000000;font-size:9px;font-family:Arial, Helvetica Neue, Helvetica, sans-serif;font-weight:400;line-height:120%;text-align:left;direction:ltr;letter-spacing:0px;mso-line-height-alt:16.8px;">
 		<p style="margin: 0;">SIZE RANGE:</p>
 		</div>
